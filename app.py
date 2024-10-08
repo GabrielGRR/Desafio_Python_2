f134fed2a0e968e657700bbb0dd682491f4f2cd5
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2024-10-08 14:58:43 -0300

    escolher entre qual tts e escolher voz menos irritante

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from pypdf import PdfReader
 import pyttsx3
+from gtts import gTTS
 
-reader = PdfReader("desafio_python_2/O Programador Pragmatico.pdf") #Caminho relativo
+reader = PdfReader("O Programador Pragmatico.pdf") # Caminho relativo
 number_of_pages = len(reader.pages) # não obrigatório
-page = reader.pages[8] # seleciona a página que você quer que seja transcrito
+n = 8
+page = reader.pages[n] # seleciona a página que você quer que seja transcrito
 text = page.extract_text()
+
+# if n+1 <= number_of_pages:
+#     buffer = reader.pages[n+1] # seleciona a página que você quer que seja transcrito
+# else:
+#     page = reader.pages[n] # seleciona a página que você quer que seja transcrito
+#     text = page.extract_text()
 
 print(text)
 
 tts = pyttsx3.init()
-tts.save_to_file(text, 'desafio_python_2/audio.mp3')
-tts.runAndWait()
+tts.save_to_file(text, 'audio.mp3')
 #tts.say(text)
+tts.runAndWait()
 
-# engine.save_to_file('Hello World' , 'test.mp3')
-# engine.runAndWait()
+# tts = gTTS(text, lang='pt')
+# tts.save('audio2.mp3')
