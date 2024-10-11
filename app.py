d9569c564914c0fa9662279113e2c33b7a3cb800
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2024-10-11 17:21:58 -0300

    adicionado os botões e as funções, mas precisa ser corrigido

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pygame
 ## Transformar PDF em TXT
 reader = PdfReader("O Programador Pragmatico.pdf") # Caminho relativo
 number_of_pages = len(reader.pages) # não obrigatório
-n = 217
+n = 215
 page = reader.pages[n] # seleciona a página que você quer que seja transcrito
 text = page.extract_text()
 print(text)
@@ -16,7 +16,7 @@ print(text)
 
 ## Text to Speech (TTS) e 
 tts = pyttsx3.init()
-tts.save_to_file(text, 'audio.mp3')
+tts.save_to_file(text, 'sound/current.mp3')
 #tts.say(text) # não salva o MP3, reproduz diretamente
 tts.runAndWait()
 
@@ -34,12 +34,8 @@ root.title("mPD player | PD_f player")
 # Definindo o tamanho inicial da janela
 root.geometry("400x300")
 
-# Criando a caixa branca (Canvas)
-canvas = tk.Canvas(root, width=150, height=100, bg='white')
-canvas.pack(side="top", padx=10, pady=10)
-
 # Função para tocar o arquivo MP3
-# def play_music():
+# def play_sound():
 #     file_path = filedialog.askopenfilename(filetypes=[("MP3 Files", "*.mp3")])
 #     if file_path:
 #         pygame.mixer.music.load(file_path)
@@ -49,13 +45,56 @@ canvas.pack(side="top", padx=10, pady=10)
 # Inicializando o pygame
 pygame.mixer.init()
 
-def play_music():
-    pygame.mixer.music.load('audio2.mp3')
-    pygame.mixer.music.play()
 
-# Botão para selecionar e tocar música
-btn_play = tk.Button(root, text="Tocar MP3", command=play_music)
-btn_play.pack(pady=20)
+## funções para os botões da interface
+def play_sound():
+    try:
+        pygame.mixer.music.load('sound/current.mp3')
+        pygame.mixer.music.play()
+    except:
+        print("play music failed")
+
+def pause_sound():
+    try:
+        pygame.mixer.music.pause()
+    except:
+        print("pause music failed")
+
+def prev_sound():
+    try:
+        pygame.mixer.music.load('sound/prev.mp3')
+    except:
+        print("prev sound failed")
+
+def next_sound():
+    try:
+        pygame.mixer.music.load('sound/next.mp3')
+    except:
+        print("next sound failed")
+
+
+# input box (escolher pagina)
+# mute
+# slider volume
+
+## Botões
+# fazer este botão virar toggle
+btn_play = tk.Button(root, text="Play", command=play_sound)
+btn_play.grid(row=1, column=1) #pady="20"
+
+btn_pause = tk.Button(root, text="Pause", command=pause_sound)
+btn_pause.grid(row=1, column=2)
+
+btn_next = tk.Button(root, text="Next", command=next_sound)
+btn_next.grid(row=1, column=3)
+
+btn_prev = tk.Button(root, text="Prev", command=prev_sound)
+btn_prev.grid(row=1, column=0)
+
+# Texto
+tk_text = tk.Label(root, text="hello world").grid(row=0,column=1)
+#tk_text = tk.Label(root, text=text).grid(row=0,column=1)
+
 
 # Executando a janela
 root.mainloop()
