bb3af7b3ad06936ca12824b868b9f655fc81765f
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2024-11-06 20:05:23 -0300

    venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\GabrielPECC\Desktop\PD\Desafio Python 2\desafio_python_2\venv
+set VIRTUAL_ENV=C:\Users\Gabriel G\Desktop\Python\PD\desafio_python_2\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
